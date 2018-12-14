9779e7b5f95c732c1f24075d9f4434ad86497120
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-14 12:31:43 -0500

    Lint and rubocop fixes

--- spec/acceptance/mcelog_spec.rb
+++ spec/acceptance/mcelog_spec.rb
@@ -2,59 +2,59 @@ require 'spec_helper_acceptance'
 
 describe 'mcelog class:' do
   context 'with default parameters' do
-    it 'should run successfully' do
-      pp =<<-EOS
+    it 'run successfully' do
+      pp = <<-EOS
       class { '::mcelog':
         service_ensure => 'stopped',
       }
       EOS
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
-    if fact('os.release.major').to_s == '6'
-      service_name = 'mcelogd'
-    else
-      service_name = 'mcelog'
-    end
+    service_name = if fact('os.release.major').to_s == '6'
+                     'mcelogd'
+                   else
+                     'mcelog'
+                   end
 
     describe package('mcelog') do
-      it { should be_installed }
+      it { is_expected.to(be_installed) }
     end
 
     describe service(service_name) do
       # Can't run mcelog in docker
-      it { should_not be_running }
-      it { should be_enabled }
+      it { is_expected.not_to(be_running) }
+      it { is_expected.to(be_enabled) }
     end
   end
 
   context 'with ensure => absent' do
-    it 'should run successfully' do
-      pp =<<-EOS
+    it 'run successfully' do
+      pp = <<-EOS
       class { '::mcelog':
         ensure => 'absent',
       }
       EOS
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
-    if fact('os.release.major').to_s == '6'
-      service_name = 'mcelogd'
-    else
-      service_name = 'mcelog'
-    end
+    service_name = if fact('os.release.major').to_s == '6'
+                     'mcelogd'
+                   else
+                     'mcelog'
+                   end
 
     describe package('mcelog') do
-      it { should_not be_installed }
+      it { is_expected.not_to(be_installed) }
     end
 
     describe service(service_name) do
-      it { should_not be_running }
-      it { should_not be_enabled }
+      it { is_expected.not_to(be_running) }
+      it { is_expected.not_to(be_enabled) }
     end
   end
 end
--- spec/classes/mcelog_spec.rb
+++ spec/classes/mcelog_spec.rb
@@ -7,54 +7,54 @@ describe 'mcelog' do
         facts
       end
 
-      if facts[:os]['release']['major'] == 6
-        service_name = 'mcelogd'
-      else
-        service_name = 'mcelog'
-      end
+      service_name = if facts[:os]['release']['major'] == 6
+                       'mcelogd'
+                     else
+                       'mcelog'
+                     end
 
       it { is_expected.to compile.with_all_deps }
 
       it { is_expected.to create_class('mcelog') }
       it { is_expected.to contain_class('mcelog::params') }
-      
+
       it do
-        is_expected.to contain_package('mcelog').with({
-          :ensure => 'present',
-          :name   => 'mcelog',
-          :before => 'File[mcelog.conf]',
-        })
+        is_expected.to contain_package('mcelog').with(
+          ensure: 'present',
+          name: 'mcelog',
+          before: 'File[mcelog.conf]',
+        )
       end
 
       it do
-        is_expected.to contain_file('mcelog.conf').with({
-          :ensure  => 'file',
-          :path    => '/etc/mcelog/mcelog.conf',
-          :owner   => 'root',
-          :group   => 'root',
-          :mode    => '0644',
-          :notify  => 'Service[mcelog]',
-        })
+        is_expected.to contain_file('mcelog.conf').with(
+          ensure: 'file',
+          path: '/etc/mcelog/mcelog.conf',
+          owner: 'root',
+          group: 'root',
+          mode: '0644',
+          notify: 'Service[mcelog]',
+        )
       end
 
       it { is_expected.to have_ini_setting_resource_count(0) }
 
       it do
-        is_expected.to contain_service('mcelog').with({
-          :ensure     => 'running',
-          :enable     => 'true',
-          :name       => service_name,
-          :hasstatus  => 'true',
-          :hasrestart => 'true',
-        })
+        is_expected.to contain_service('mcelog').with(
+          ensure: 'running',
+          enable: 'true',
+          name: service_name,
+          hasstatus: 'true',
+          hasrestart: 'true',
+        )
       end
 
       context 'settings defined' do
         let(:params) do
           {
-            :settings => {
+            settings: {
               '' => { 'filter' => 'yes' },
-              'server' => {'client-user' => 'root' },
+              'server' => { 'client-user' => 'root' },
             },
           }
         end
@@ -62,40 +62,40 @@ describe 'mcelog' do
         it { is_expected.to have_ini_setting_resource_count(2) }
 
         it do
-          is_expected.to contain_ini_setting('/etc/mcelog/mcelog.conf [] filter').with({
-            :ensure  => 'present',
-            :section => '',
-            :setting => 'filter',
-            :value   => 'yes',
-            :path    => '/etc/mcelog/mcelog.conf',
-            :require => 'Package[mcelog]',
-            :notify  => 'Service[mcelog]',
-          })
+          is_expected.to contain_ini_setting('/etc/mcelog/mcelog.conf [] filter').with(
+            ensure: 'present',
+            section: '',
+            setting: 'filter',
+            value: 'yes',
+            path: '/etc/mcelog/mcelog.conf',
+            require: 'Package[mcelog]',
+            notify: 'Service[mcelog]',
+          )
         end
 
         it do
-          is_expected.to contain_ini_setting('/etc/mcelog/mcelog.conf [server] client-user').with({
-            :ensure  => 'present',
-            :section => 'server',
-            :setting => 'client-user',
-            :value   => 'root',
-            :path    => '/etc/mcelog/mcelog.conf',
-            :require => 'Package[mcelog]',
-            :notify  => 'Service[mcelog]',
-          })
+          is_expected.to contain_ini_setting('/etc/mcelog/mcelog.conf [server] client-user').with(
+            ensure: 'present',
+            section: 'server',
+            setting: 'client-user',
+            value: 'root',
+            path: '/etc/mcelog/mcelog.conf',
+            require: 'Package[mcelog]',
+            notify: 'Service[mcelog]',
+          )
         end
       end
 
       context 'ensure => absent' do
-        let(:params) {{ :ensure => 'absent' }}
+        let(:params) { { ensure: 'absent' } }
 
-        it { should contain_package('mcelog').with_ensure('absent') }
-        it { should contain_file('mcelog.conf').with_ensure('absent') }
+        it { is_expected.to contain_package('mcelog').with_ensure('absent') }
+        it { is_expected.to contain_file('mcelog.conf').with_ensure('absent') }
         it do
-          should contain_service('mcelog').with({
-            :ensure => 'stopped',
-            :enable => 'false',
-          })
+          is_expected.to contain_service('mcelog').with(
+            ensure: 'stopped',
+            enable: 'false',
+          )
         end
       end
     end
