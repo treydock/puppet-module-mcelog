b866d09e2db76bc6d417b13cbb34bda948cf6b24
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-14 12:41:55 -0500

    pdk update

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,7 +4,7 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 dir = File.expand_path(File.dirname(__FILE__))
-Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
+Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each { |f| require f }
 
 run_puppet_install_helper
 install_module_on(hosts)
